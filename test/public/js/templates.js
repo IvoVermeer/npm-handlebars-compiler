2b36e82e3812944c83cc60f589b771dfd885a616
Ivo Vermeer <ivovermeer@gmail.com>
2016-10-19 21:37:20 +0200

    Removed the test-generated templates.js

--- test/public/js/templates.js
+++
@@ -1,9 +0,0 @@
-(function() {
-  var template = Handlebars.template, templates = Handlebars.templates = Handlebars.templates || {};
-templates['testExtraTemplate'] = template({"compiler":[7,">= 4.0.0"],"main":function(container,depth0,helpers,partials,data) {
-    return "some text";
-},"useData":true});
-templates['testTemplate'] = template({"compiler":[7,">= 4.0.0"],"main":function(container,depth0,helpers,partials,data) {
-    return "<p>I was compiled</p>";
-},"useData":true});
-})();
